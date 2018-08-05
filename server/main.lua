fed46c81bb2f96e20faad8abdedb0c1a09a4574c
leonardo emanuele <manups4e@gmail.com>
2018-08-05 12:56:00 +0200

    added Player checks to prevent errors

--- server/main.lua
+++ server/main.lua
@@ -10,7 +10,9 @@ end)
 
 ESX.RegisterServerCallback('esx_spectate:getPlayerData', function(source, cb, id)
 	local xPlayer = ESX.GetPlayerFromId(id)
-	cb(xPlayer)
+	if xPlayer ~= nil then
+	   cb(xPlayer)
+	end	
 end)
 
 RegisterServerEvent('esx_spectate:kick')
@@ -21,39 +23,40 @@ end)
 ESX.RegisterServerCallback('esx_spectate:getOtherPlayerData', function(source, cb, target)
 
     local xPlayer = ESX.GetPlayerFromId(target)
+    if xPlayer ~= nil then
+       local identifier = GetPlayerIdentifiers(target)[1]
 
-    local identifier = GetPlayerIdentifiers(target)[1]
+       local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
+            ['@identifier'] = identifier
+            })
 
-    local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
-      ['@identifier'] = identifier
-    })
+       local user      = result[1]
+       local firstname     = user['firstname']
+       local lastname      = user['lastname']
+       local sex           = user['sex']
+       local dob           = user['dateofbirth']
+       local height        = user['height'] .. " Centimetri"
+	   local money			= user['money']
+           local bank			= user['bank']
 
-    local user      = result[1]
-    local firstname     = user['firstname']
-    local lastname      = user['lastname']
-    local sex           = user['sex']
-    local dob           = user['dateofbirth']
-    local height        = user['height'] .. " Centimetri"
-	local money			= user['money']
-	local bank			= user['bank']
+       local data = {
+         name        = GetPlayerName(target),
+         job         = xPlayer.job,
+         inventory   = xPlayer.inventory,
+         accounts    = xPlayer.accounts,
+         weapons     = xPlayer.loadout,
+         firstname   = firstname,
+         lastname    = lastname,
+         sex         = sex,
+         dob         = dob,
+         height      = height,
+	     money 	  = money,
+	     bank 		  = bank
+       }
 
-    local data = {
-      name        = GetPlayerName(target),
-      job         = xPlayer.job,
-      inventory   = xPlayer.inventory,
-      accounts    = xPlayer.accounts,
-      weapons     = xPlayer.loadout,
-      firstname   = firstname,
-      lastname    = lastname,
-      sex         = sex,
-      dob         = dob,
-      height      = height,
-	  money 	  = money,
-	  bank 		  = bank
-    }
-
-      TriggerEvent('esx_license:getLicenses', target, function(licenses)
-        data.licenses = licenses
-        cb(data)
-	end)
+         TriggerEvent('esx_license:getLicenses', target, function(licenses)
+           data.licenses = licenses
+           cb(data)
+	   end)
+    end
 end)
