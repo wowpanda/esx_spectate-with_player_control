1b37e229ca3b86a924615278e36bd2507ddc5085
Søren Eskildsen <soeren@bzn.dk>
2019-04-09 02:42:13 +0200

    Revert "Security Update"
    This reverts commit d34648ce2a3f9a86ce8bc88e4e16ed0a5bbbd805.

--- server/main.lua
+++ server/main.lua
@@ -17,14 +17,10 @@ end)
 
 RegisterServerEvent('esx_spectate:kick')
 AddEventHandler('esx_spectate:kick', function(target, msg)
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	if xPlayer.getGroup() ~= 'user' then
-		DropPlayer(target, msg)
-	else
-		print(('esx_spectate: %s attempted to kick a player!'):format(xPlayer.identifier))
-		DropPlayer(source, "esx_spectate: you're not authorized to kick people dummy.")
-	end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.player.getGroup() ~= 'user' or xPlayer.player.getGroup() ~= nil then
+        DropPlayer(target, msg)
+    end
 end)
 
 ESX.RegisterServerCallback('esx_spectate:getOtherPlayerData', function(source, cb, target)
